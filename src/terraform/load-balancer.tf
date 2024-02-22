3e64de8de74c4fcf835f690755b7b099f1262272
markti <markti@outlook.com>
2024-02-21 22:13:43 -0500

    alb

--- src/terraform/load-balancer.tf
+++ src/terraform/load-balancer.tf
@@ -1,10 +1,45 @@
-/*
+locals {
+  subnet_ids = [for subnet in values(aws_subnet.backend) : subnet.id]
+}
+
+resource "aws_security_group" "alb" {
+  name        = "alb-${var.application_name}-${var.environment_name}"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    application = var.application_name
+    environment = var.environment_name
+  }
+}
+
 resource "aws_lb" "main" {
-  name               = "my-alb"
+  name               = "alb-${var.application_name}-${var.environment_name}"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.my_alb_sg.id]
-  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = local.subnet_ids
 
   enable_deletion_protection = false
 
@@ -14,4 +49,3 @@ resource "aws_lb" "main" {
     environment = var.environment_name
   }
 }
-*/
