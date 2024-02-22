1fab82d039b1246361e1f87e92483d91954fbf74
markti <markti@outlook.com>
2024-02-21 19:29:53 -0500

    add terraform pipeline

---
+++ src/terraform/lambda.tf
@@ -0,0 +1,23 @@
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name               = "${var.application_name}-${var.environment_name}-lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda.json
+}
+
+resource "aws_lambda_function" "main" {
+  function_name = "${var.application_name}-${var.environment_name}"
+  role          = aws_iam_role.lambda.arn
+  runtime       = "dotnet6"
+}
---
+++ src/terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+application_name  = "fleet-portal"
+environment_name  = "serverless"
+primary_region    = "us-west-2"
+ecr_image_pushers = ["Terraform"]
