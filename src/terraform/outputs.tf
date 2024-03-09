7841dc90d81b181f4898dcf489abdaa4d74a9f61
markti <markti@outlook.com>
2024-03-09 10:08:10 -0500

    aws region frontend_bucket_name

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,3 +1,6 @@
-output "bucket_domain_name" {
-  value = aws_s3_bucket.frontend.bucket_domain_name
+output "frontend_bucket_name" {
+  value = aws_s3_bucket.frontend.bucket
+}
+output "frontend_bucket_region" {
+  value = aws_s3_bucket.frontend.bucket
 }
