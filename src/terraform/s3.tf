4ff9a60a6542630e5424bf2e439f9eff7f594711
markti <markti@outlook.com>
2024-03-09 09:17:09 -0500

    website config

--- src/terraform/s3.tf
+++ src/terraform/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "frontend" {
 }
 
 resource "aws_s3_bucket_website_configuration" "frontend" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.frontend.id
 
   index_document {
     suffix = "index.html"
