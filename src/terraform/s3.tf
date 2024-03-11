1ca77a702edaffc31c783e9a564cb2d5bcc0e0ee
markti <markti@outlook.com>
2024-03-10 22:08:18 -0400

    block public access

--- src/terraform/s3.tf
+++ src/terraform/s3.tf
@@ -8,6 +8,15 @@ resource "aws_s3_bucket" "frontend" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_website_configuration" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
