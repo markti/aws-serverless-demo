8392177dde05038ed4aad250e17bebd5d23fb6c6
markti <markti@outlook.com>
2024-03-17 18:29:35 -0400

    respoinse

--- src/dotnet/backend/FleetAPI/LambdaResponse.cs
+++ src/dotnet/backend/FleetAPI/LambdaResponse.cs
@@ -3,6 +3,8 @@ namespace FleetAPI;
 
 public class LambdaResponse {
     public int StatusCode {get;set;}
+    public string StatusDescription {get;set;}
     public IDictionary<string, string> Headers {get;set;}
     public string Body {get;set;}
+    public bool isBase64Encoded { get; set; }
 }
