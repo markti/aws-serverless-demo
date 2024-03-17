7da540c71e65b9d0ca4afcdf07d2be716ca769ba
markti <markti@outlook.com>
2024-03-17 13:26:54 -0400

    feather forecast

---
+++ src/dotnet/backend/FleetAPI/WeatherForecastFunction.cs
@@ -0,0 +1,36 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace FleetAPI;
+
+public class WeatherForecast {
+    public DateTime Date {get;set;}
+    public int TemperatureC {get;set;}
+    public string Summary {get;set;}
+}
+
+public class Function
+{
+
+    private static readonly string[] Summaries = new [] {
+        "Freezing", "Bracing", "Chilly"
+    }
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public IEnumerable<WeatherForecast> FunctionHandler(ILambdaContext context)
+    {
+        return Enumerable.Range(1,5).Select(index => new WeatherForecast {
+            Date = DateTime.Now.Adddays(index),
+            TemperatureC = Random.Shared.Next(-20,55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
